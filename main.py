57d02ff3f03f095808c3767c3b9fa13b6e046a36
stephen0804 <stephenevans0804@hotmail.com>
2025-04-18 17:39:07 +0930

    CH2 L5 add player.py Player class and draw play

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import Player
 
 
 def main():
@@ -10,11 +11,15 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     time = pygame.time.Clock()
     dt = 0
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x, y)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill(("black"))
+        player.draw(screen)
         pygame.display.flip()
         dt = time.tick(60) / 1000
 
